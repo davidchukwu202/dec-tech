5e75fe388c47d3549e24faaef45703d60fada10d
chukwu chukwuebuka david <davidchukwu202@gmail.com>
2025-06-24 16:07:16 +0100

    backend

--- utils/calculatorLogic.js
+++
@@ -1,8 +0,0 @@
-const calculateBattery = (loadWatts, hours, voltage, dod) => {
-  const usableWh = loadWatts * hours;
-  const totalAh = usableWh / voltage;
-  const adjustedAh = totalAh / dod;
-  return Math.cell(adjustedAh);
-};
-
-export default calculateBattery;
